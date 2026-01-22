aecf38b877dfebb9bd13a9fa9acd90c9a1f7ea15
Jacob S <jacobslearning12@gmail.com>
2026-01-22 22:58:32 +0000

    security fix

--- app.py
+++ app.py
@@ -31,4 +31,6 @@ def page_not_found(e):
     return redirect('/')
 
 if __name__ == '__main__':
-   app.run(host="0.0.0.0", port=5000)
+    host = os.getenv("FLASK_HOST", "127.0.0.1")
+    port = int(os.getenv("FLASK_PORT", 5000))
+    app.run(host=host, port=port)
